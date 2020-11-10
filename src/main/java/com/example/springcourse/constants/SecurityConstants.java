294a2d3a9cfdc6b0c5243e16a43cccb0234aa654
Mateus Hernani <mateus@technosoftware.com.br>
2020-11-09 22:33:18 -0300

    Create Security constants

---
+++ src/main/java/com/example/springcourse/constants/SecurityConstants.java
@@ -0,0 +1,10 @@
+package com.example.springcourse.constants;
+
+public class SecurityConstants {
+	
+	public static final int JWT_EXPIRATION_DAYS = 5;
+	public static final String JWT_PROVIDER = "Bearer ";
+	public static final String API_KEY = "spring-course";
+	public static final String JWT_ROLE_KEY = "role";
+
+}
