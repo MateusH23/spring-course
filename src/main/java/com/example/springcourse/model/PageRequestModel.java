0e6b72e6d935a7a9476d5072f697beeaa77b0ecc
Mateus Hernani <mateus@technosoftware.com.br>
2020-11-15 15:49:12 -0300

    Add logic for sort param

--- src/main/java/com/example/springcourse/model/PageRequestModel.java
+++ src/main/java/com/example/springcourse/model/PageRequestModel.java
@@ -1,8 +1,12 @@
 package com.example.springcourse.model;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,14 +21,32 @@ public class PageRequestModel {
 	
 	private int page = 0;
 	private int size = 10;
+	private String sort = "";
 	
 	public PageRequestModel(Map<String, String> params) {
 		if(params.containsKey("page")) page = Integer.parseInt(params.get("page"));
 		if(params.containsKey("size")) size = Integer.parseInt(params.get("size"));
+		if(params.containsKey("sort")) sort = params.get("sort");
 	}
 	
 	public PageRequest toSpringPageRequest() {
-		return PageRequest.of(page, size);
+		List<Order> orders = new ArrayList<>();
+		String[] props = sort.split(",");
+		
+		for (String prop : props) {
+			prop = prop.trim();
+			if(prop.isEmpty())
+				continue;
+			
+			if(prop.startsWith("-")) {
+				prop = prop.replace("-", "");
+				orders.add(Order.desc(prop));
+			} else {
+				orders.add(Order.asc(prop));
+			}
+		}
+		
+		return PageRequest.of(page, size, Sort.by(orders));
 	}
 
 }
